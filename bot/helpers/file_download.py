df4de309407da8f67805ad34f93a96bc79a9e902
bishal-dahal <101317575+bishaldahal@users.noreply.github.com>
2024-04-28 20:55:40 +0545

    chore: Refactor file_download.py for improved readability and error handling

--- bot/helpers/file_download.py
+++ bot/helpers/file_download.py
@@ -7,16 +7,14 @@ logger = LOGGER(__name__)
 
 
 async def download_media(client: CloudBot, message: CallbackQuery ) -> str:
-    # print("CL: ", client)
-    # print("Mess: ",message)
     user_message = await client.edit_message_text(
         chat_id=message.from_user.id,
         message_id=message.message.id,
-        text="processing your request...",
+        text="Processing your request...",
     )
     try:
         media_id = message.message.reply_to_message
-        await user_message.edit_text("downloading started...")
+        await user_message.edit_text("Downloading started...")
         download_file_path = await client.download_media(media_id)
         return download_file_path
     except RPCError as e:
