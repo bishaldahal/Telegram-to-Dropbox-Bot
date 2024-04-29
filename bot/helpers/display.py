82ba410923368c51d6a7a036668ebd55fff4a9bc
bishal-dahal <101317575+bishaldahal@users.noreply.github.com>
2024-04-29 07:22:14 +0545

    Add download progess bar

--- bot/helpers/display.py
+++ bot/helpers/display.py
@@ -2,7 +2,13 @@
 
 
 from pyrogram.types import Message
+import time
+from ..helpers.timedata import time_data
+from bot import LOGGER
+logger = LOGGER(__name__)
 
+
+start_time = time.time()
 
 async def progress(current, total, message: Message, ):
     if current is total:
@@ -13,37 +19,39 @@ async def progress(current, total, message: Message, ):
         text=f"start downloading..."
     )
 
-    # time_now = time.time()
-    # time_diff = time_now - start_time
-    # percent = round(current * 100 // total)
-    # progress_ = "{0} {1}%".format(
-    #     progressBar(percent),
-    #     f"{current * 100 / total:.1f}"
-    # )
-    # time_ = "Time: {0}".format(
-    #     time_data(start_time)
-    # )
-    #
-    # speed_ = "Speed: {0}".format(
-    #     human_readable_size(size=current / time_diff)
-    # )
-    # download_ = "{0} of {1}".format(
-    #     human_readable_size(current),
-    #     human_readable_size(total)
-    # )
-    # try:
-    #
-    #     await message.edit(
-    #         text=f"{progress_}\n{download_}\n{speed_}    {time_}"
-    #     )
-    #
-    # except Exception as e:
-    #     await message.edit(
-    #         text=f"ERROR: {e}"
-    #     )
+    time_now = time.time()
+    time_diff = time_now - start_time
+    percent = round(current * 100 // total)
+    progress_ = "{0} {1}%".format(
+        progressBar(percent),
+        f"{current * 100 / total:.1f}"
+    )
+    time_ = "Time: {0}".format(
+        time_data(start_time)
+    )
+    
+    speed_ = "Speed: {0}".format(
+        human_readable_size(size=current / time_diff)
+    )
+    download_ = "{0} of {1}".format(
+        human_readable_size(current),
+        human_readable_size(total)
+    )
+    try:
+    
+        await message.edit(
+            text=f"{progress_}\n{download_}\n{speed_}    {time_}"
+        )
+    
+    except Exception as e:
+        await message.edit(
+            text=f"ERROR: {e}"
+        )
+        logger.error(f"Error updating download progress: {e}")
 
 
-def progressbar(percent):
+
+def progressBar(percent):
     done_block = '█'
     empty_block = '░'
     return f"{done_block * int(percent / 5)}{empty_block * int(20 - int(percent / 5))}"
--- bot/helpers/file_download.py
+++ bot/helpers/file_download.py
@@ -2,11 +2,11 @@ from pyrogram.types import CallbackQuery
 from pyrogram.errors import RPCError
 from bot import LOGGER
 from ..filetocloud import CloudBot
+from .display import progress
 
 logger = LOGGER(__name__)
 
-
-async def download_media(client: CloudBot, message: CallbackQuery ) -> str:
+async def download_media(client: CloudBot, message: CallbackQuery) -> str:
     user_message = await client.edit_message_text(
         chat_id=message.from_user.id,
         message_id=message.message.id,
@@ -15,7 +15,11 @@ async def download_media(client: CloudBot, message: CallbackQuery ) -> str:
     try:
         media_id = message.message.reply_to_message
         await user_message.edit_text("Downloading started...")
-        download_file_path = await client.download_media(media_id)
+        download_file_path = await client.download_media(
+            media_id,
+            progress=progress,
+            progress_args=(user_message,)  # Adjusted to pass user_message correctly
+        )
         return download_file_path
     except RPCError as e:
         logger.error(e)
